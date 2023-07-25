dc2053ae517536e74f974761c0f34c2401d1dc08
6Neel9 <nilmandal300@gmail.com>
2023-07-25 11:16:27 +0530

    2nd commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import MapExample from "./MapExample";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <MapExample />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/MapExample.jsx
@@ -0,0 +1,55 @@
+import  { useEffect, useState } from 'react';
+import { MapContainer as Map, TileLayer } from 'react-leaflet';
+import HeatmapLayer from './HeatmapLayer';
+
+const addressPoints = [
+    [40.7128, -74.0060, 0.6], // New York City, NY, USA
+    [34.0522, -118.2437, 0.8], // Los Angeles, CA, USA
+    [51.5074, -0.1278, 0.4], // London, UK
+    [48.8566, 2.3522, 0.7], // Paris, France
+    [35.6895, 139.6917, 0.9], // Tokyo, Japan
+    [55.7558, 37.6176, 0.5], // Moscow, Russia
+    [52.5200, 13.4050, 0.3], // Berlin, Germany
+    [41.8781, -87.6298, 0.6], // Chicago, IL, USA
+    [22.5726, 88.3639, 0.8], // Kolkata, India
+    [-33.8688, 151.2093, 0.4], // Sydney, Australia
+    [-23.5505, -46.6333, 0.7], // Sao Paulo, Brazil
+    [-36.8485, 174.7633, 0.9], // Auckland, New Zealand
+    [55.7558, 12.4376, 0.5], // Copenhagen, Denmark
+    [37.7749, -122.4194, 0.3], // San Francisco, CA, USA
+  ];
+  
+
+const MapExample = () => {
+  const [mapCenter, setMapCenter] = useState([0, 0]);
+
+  useEffect(() => {
+    // Generate random latitude and longitude
+    const randomLatitude = (Math.random() * 180) - 90;
+    const randomLongitude = (Math.random() * 360) - 180;
+
+    // Update the map center with the random latitude and longitude
+    setMapCenter([randomLatitude, randomLongitude]);
+  }, []); // Empty dependency array to run the effect only once on mount
+
+  return (
+    <div>
+      <Map center={mapCenter} zoom={2}>
+        <HeatmapLayer
+          fitBoundsOnLoad
+          fitBoundsOnUpdate
+          points={addressPoints}
+          longitudeExtractor={m => m[1]}
+          latitudeExtractor={m => m[0]}
+          intensityExtractor={m => parseFloat(m[2])}
+        />
+        <TileLayer
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </Map>
+    </div>
+  );
+};
+
+export default MapExample;
